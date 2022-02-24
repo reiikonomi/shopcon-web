c5c689276575e42c91abee5f3e162ca17c03bda2
Rei Ikonomi <94234882+onomdev@users.noreply.github.com>
2022-02-24 19:27:27 +0100

    Update vars.dart

--- lib/vars.dart
+++ lib/vars.dart
@@ -1,35 +1,35 @@
 class Variables {
-  static String firebaseApiKey = "AIzaSyBou5gS1sKI5C_ey2j8XfO1vVl8oDJ--bc";
+  static String firebaseApiKey = "";
 
-  static String firebaseAuthDomain = "mistreci-77a8a.firebaseapp.com";
+  static String firebaseAuthDomain = "";
 
-  static String firebaseProjectId = "mistreci-77a8a";
+  static String firebaseProjectId = "";
 
-  static String firebaseStorageBucket = "mistreci-77a8a.appspot.com";
+  static String firebaseStorageBucket = "";
 
-  static String firebaseMessagingSenderId = "382100351557";
+  static String firebaseMessagingSenderId = "";
 
-  static String firebaseAppId = "1:382100351557:web:081f0f4fef8ed78cd0512c";
+  static String firebaseAppId = "";
 
-  static String firebaseMeasurementId = "G-953CGD9TS7";
+  static String firebaseMeasurementId = "";
 
   static String searchProductApiEndoint =
-      "https://shopconal.herokuapp.com/api/v1/mistreci/apikey=7fab6989Waf21t472bfafd4183075150de62/categories/marketplace/products/search/";
+      "https://shopconal.herokuapp.com/api/v1/mistreci/apikey=?/categories/marketplace/products/search/";
 
   static String searchJobsApiEndpoint =
-      "https://shopconal.herokuapp.com/api/v1/mistreci/apikey=7fab6989Waf21t472bfafd4183075150de62/categories/jobs/search/";
+      "https://shopconal.herokuapp.com/api/v1/mistreci/apikey=?/categories/jobs/search/";
   static String jobCategoryApiEndpoint =
-      "https://shopconal.herokuapp.com/api/v1/mistreci/apikey=7fab6989Waf21t472bfafd4183075150de62/categories/jobs/categories/";
+      "https://shopconal.herokuapp.com/api/v1/mistreci/apikey=?/categories/jobs/categories/";
   static String productCategoryApiEndpoint =
-      "https://shopconal.herokuapp.com/api/v1/mistreci/apikey=7fab6989Waf21t472bfafd4183075150de62/categories/marketplace/products/categories/";
+      "https://shopconal.herokuapp.com/api/v1/mistreci/apikey=?/categories/marketplace/products/categories/";
   static String postJob =
-      "https://shopconal.herokuapp.com/api/v1/mistreci/apikey=7fab6989Waf21t472bfafd4183075150de62/categories/jobs/";
+      "https://shopconal.herokuapp.com/api/v1/mistreci/apikey=?/categories/jobs/";
   static String postProduct =
-      "https://shopconal.herokuapp.com/api/v1/mistreci/apikey=7fab6989Waf21t472bfafd4183075150de62/categories/marketplace/products";
+      "https://shopconal.herokuapp.com/api/v1/mistreci/apikey=?/categories/marketplace/products";
   static String imageApiEndpoint =
-      "https://shopconal.herokuapp.com/uploads/apikey=7fab6989Waf21t472bfafd4183075150de62/";
+      "https://shopconal.herokuapp.com/uploads/apikey=?/";
   static String getAllJobsApiEndpoint =
-      "https://shopconal.herokuapp.com/api/v1/mistreci/apikey=7fab6989Waf21t472bfafd4183075150de62/categories/jobs/";
+      "https://shopconal.herokuapp.com/api/v1/mistreci/apikey=?/categories/jobs/";
   static String getAllProductsApiEndpoint =
-      "https://shopconal.herokuapp.com/api/v1/mistreci/apikey=7fab6989Waf21t472bfafd4183075150de62/categories/marketplace/products/";
+      "https://shopconal.herokuapp.com/api/v1/mistreci/apikey=?/categories/marketplace/products/";
 }
